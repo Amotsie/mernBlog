656cbd752088b84bbb600c96490a71b2c0582464
Amotsie <motsie.at@gmail.com>
2021-02-21 14:45:33 +0300

    fixed static resources path

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //Deployment options
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 }
 
 //HTTP request logger
